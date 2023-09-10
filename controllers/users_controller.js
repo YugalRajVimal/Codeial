3f0e0d3f265b9f76ebf6acac01135675911e5273
YugalRajVimal <sagaryugal472@gmail.com>
2023-09-10 17:30:08 +0530

    Implemented Munual Authentication

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -6,36 +6,108 @@
 
 const users = require('../models/user')
 
-module.exports.profile = (req, res)=>{
-    return res.render('user_profile', {
-        title: 'User Profile'
-    });
+module.exports.profile = async (req, res)=>{
+    if(req.cookies.user_id){
+        try {
+            let user = await users.findById(req.cookies.user_id);
+            if(user){
+                return res.render('user_profile', {
+                    title: 'User Profile',
+                    name: user.name
+                });
+            }
+            return res.redirect('/users/sign-in');
+        } catch (error) {
+            console.log("Error in accessing profile");
+        }
+    }
+    else{
+        return res.redirect('/users/sign-in');
+    }
 };
 
 // Render the Sign Up page
-module.exports.signUp = (req,res)=>{
-    return res.render('user_sign_up',{
-        title:"Codeial" | "SignUp"
-    });
+module.exports.signUp = async (req,res)=>{
+
+    if(req.cookies.user_id){
+        try {
+            let user = await users.findById(req.cookies.user_id);
+            if(user){
+                return res.redirect('/users/profile');
+            }
+            return res.render('user_sign_up',{
+                title:"Codeial" | "SignUp"
+            });
+        } catch (error) {
+            console.log("Error in SignUp");
+        }
+    }
+    else{
+        return res.render('user_sign_up',{
+            title:"Codeial" | "SignUp"
+        });
+    }
+
+    // return res.render('user_sign_up',{
+    //     title:"Codeial" | "SignUp"
+    // });
 };
 
 // Render the Sign In page
-module.exports.signIn = (req,res)=>{
-    return res.render('user_sign_in',{
-        title:"Codeial" | "SignIn"
-    });
+module.exports.signIn =async (req,res)=>{
+    if(req.cookies.user_id){
+        try {
+            let user = await users.findById(req.cookies.user_id);
+            if(user){
+                return res.redirect('/users/profile');
+            }
+            return res.render('user_sign_in',{
+                title:"Codeial-SignIn"
+            });
+        } catch (error) {
+            console.log("Error in SignIn");
+        }
+    }
+    else{
+        return res.render('user_sign_in',{
+            title:"Codeial-SignIn"
+        });
+    }
+    
+};
+
+module.exports.signOut =async (req,res)=>{
+
+    if(req.cookies.user_id){
+        try {
+            let user =await users.findById(req.cookies.user_id);
+
+            if(user){
+                res.clearCookie('user_id');
+                return res.redirect('/users/sign-in');
+            }
+        } catch (error) {
+            console.log("Error in Sign Out",error);
+        }
+    }
+    else{
+        return res.redirect('/users/sign-in');
+    }
+    
 };
 
 // Get the sign up data
 module.exports.create = async function(req,res){
 
+    // Check whether both password and confirm_password are same or not
     if(req.body.password != req.body.confirm_password){
         return res.redirect('back');
     }
 
     try {
+        //Find user if already exist or not
         let user = await users.findOne({email:req.body.email});
-        try {
+            //User not exist
             if(!user){
                 await users.create(req.body);
                 //OR
@@ -48,13 +120,11 @@ module.exports.create = async function(req,res){
                 console.log("New user's account created")
                 return res.redirect('/users/sign-in');
             }
+            //User exist
             else{
                 console.log("Account already exist ")
                 return res.redirect('back');
             }
-        } catch (error) {
-            console.log("Error in finding user in DB");
-        }
     } catch (error) {
         console.log("Error in finding user in DB");
         return;
@@ -62,6 +132,26 @@ module.exports.create = async function(req,res){
 
 }
 
-module.exports.createSession = function(req,res){
-    //To Do
+module.exports.createSession = async function(req,res){
+   
+    try {
+        //Find User
+        let user = await users.findOne({email:req.body.email});
+
+        //User not exist
+        if(!user){
+            console.log("User does not exist");
+            return res.redirect('back');
+        }
+        else{
+            if(user.password === req.body.password){
+                res.cookie('user_id',user._id);
+                return res.redirect('/users/profile');
+            }
+        }
+    } catch (error) {
+        console.log("Error in finding user from DB")
+        return;
+    }
 }
+
