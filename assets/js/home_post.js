e12b589d63a3ad5c81fbb06bfabea8bfa1d74d42
YugalRajVimal <sagaryugal472@gmail.com>
2024-02-14 03:34:09 +0530

    Implemented AJAX in adding and deleting Post.

--- assets/js/home_post.js
+++ assets/js/home_post.js
@@ -1,61 +1,61 @@
+// Method to submit the form data for new post using AJAX
+let createPost = function () {
+  let newPostForm = $("#new-post-form");
+  newPostForm.submit(function (e) {
+    e.preventDefault();
 
-    // Method to submit the form data for new post using AJAX
-    let createPost= function(){
-        let newPostForm = $('#new-post-form');
-        newPostForm.submit(function(e){
-            e.preventDefault();
+    $.ajax({
+      type: "POST",
+      url: "/posts/create-post",
+      data: newPostForm.serialize(),
+      success: async function (data) {
+        // console.log("aa", data.data.post);
+        let populatedPost = await populateUserDetails(data.data.post);
+        let newPost = newPostDom(populatedPost);
+        $("#feed>ul").prepend(newPost);
+        deletePost($(" #delete-post-button", newPost));
+      },
+      error: function (err) {
+        console.log(err.responseText);
+      },
+    });
+  });
+};
 
-            $.ajax({
-                type:'POST',
-                url:'/posts/create-post',
-                data: newPostForm.serialize(),
-                success:async function(data){
-                    console.log("aa",data.data.post);
-                    let populatedPost = await populateUserDetails(data.data.post);
-                    let newPost= newPostDom(populatedPost);
-                    $('#feed>ul').prepend(newPost);
-                },
-                error: function(err){
-                    console.log(err.responseText);
-                }
-            })
-        })
-    }
+let populateUserDetails = async function (post) {
+  try {
+    // Assuming there's an endpoint to get user details based on the user ID
 
-    let populateUserDetails = async function (post) {
-        try {
-            // Assuming there's an endpoint to get user details based on the user ID
+    const userData = await $.ajax({
+      type: "GET",
+      url: `/users/get-user?id=${post.user}`,
+    });
 
-            const userData = await $.ajax({
-                type: 'GET',
-                url: `/users/get-user?id=${post.user}`,
-            });
-    
-            post.user = userData; // Update the 'user' object in the post with user details
-            return post;
+    post.user = userData; // Update the 'user' object in the post with user details
+    return post;
 
-            // const userData = await users.findById(post.user);
-    
-            // post.user = userData; // Replace the 'user' object in the post with user details
-            // return post;
-        } catch (error) {
-            console.error('Error fetching user details:', error);
-            return post; // Return the original post object if there's an error
-        }
-    }
+    // const userData = await users.findById(post.user);
 
-    // Method to create a post in DOM
-    let newPostDom = function(post){
-        return $(`
+    // post.user = userData; // Replace the 'user' object in the post with user details
+    // return post;
+  } catch (error) {
+    console.error("Error fetching user details:", error);
+    return post; // Return the original post object if there's an error
+  }
+};
+
+// Method to create a post in DOM
+let newPostDom = function (post) {
+  return $(`
             <li id="post-${post._id}">
                 <div id="post">
                     <div id="users_name">
-                        <h5> ${ post.user.name } </h5>
+                        <h5> ${post.user.name} </h5>
                     </div>
                     <div id="content">
-                        <h3> ${ post.content } </h3>
+                        <h3> ${post.content} </h3>
                         <div id="post-image">
-                            <img src="${ post.image }" alt="" height="400px">
+                            <img src="${post.image}" alt="" height="400px">
                         </div>
                     </div>
                     <br>
@@ -64,7 +64,7 @@
                             <a href=""> <i class="fa-regular fa-heart"></i> </a>
                         </div>
                         <div id="comment">
-                                <form action="/comments/create-comment/?id=${ post._id }" method="POST">
+                                <form action="/comments/create-comment/?id=${post._id}" method="POST">
                                     <textarea name="content" id="comment-textarea" cols="20" rows="1"></textarea>
                                     <button type="submit">
                                         <i class="fa-regular fa-comment"></i>
@@ -73,7 +73,7 @@
                         </div>
                         <div>
                             <div id="post-delete-button">
-                                    <a id="delete-post-button" href="/posts/delete-post/?id=${ post._id }">
+                                    <a id="delete-post-button" href="/posts/delete-post/?id=${post._id}">
                                         <i class="fa-solid fa-trash"></i>
                                     </a>
                             </div>
@@ -82,8 +82,24 @@
                 </div>
             </li>
         `);
-    }
+};
 
-    createPost();
+//Method to delete a post from DOM
+let deletePost = function (deleteLink) {
+  $(deleteLink).click(function (e) {
+    e.preventDefault();
 
+    $.ajax({
+      type: "get",
+      url: $(deleteLink).prop("href"),
+      success: function (data) {
+        $(`#post-${data.data.post_id}`).remove();
+      },
+      error: function (error) {
+        console.log(error.responseText);
+      },
+    });
+  });
+};
 
+createPost();
--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,55 +1,61 @@
-
-const posts = require('../models/post');
-const comments = require('../models/comment');
+const posts = require("../models/post");
+const comments = require("../models/comment");
 
 // const ObjectId = require("mongodb").ObjectId;
 
+module.exports.createPost = async function (req, res) {
+  console.log("Post Creating");
+  try {
+    // console.log("Request User",req.user);
+    console.log(req.body);
+    let post = await posts.create({
+      content: req.body.content,
+      image: req.body.image,
+      // user:await req.user._id  //(Not Working - Doubt)
+      user: await res.locals.user._id,
+    });
 
-module.exports.createPost = async function(req,res){
-    console.log("Post Creating");
-    try {
-        // console.log("Request User",req.user);
-        console.log(req.body);
-        let post = await posts.create({
-            content: req.body.content,
-            image:req.body.image,
-            // user:await req.user._id  //(Not Working - Doubt)
-            user:await res.locals.user._id
+    if (req.xhr) {
+      return res.status(200).json({
+        data: {
+          post: post,
+        },
+        message: "Post Created!",
+      });
+    }
+
+    return res.redirect("back");
+  } catch (error) {
+    console.log("Error in creating Post :", error);
+    return;
+  }
+};
+
+module.exports.deletePost = async function (req, res) {
+  try {
+    let post = await posts.find({ _id: req.query.id });
+    if (post[0].user.toString() == res.locals.user.id) {
+      await post[0].deleteOne({ _id: req.query.id });
+      await comments.deleteMany({ post: req.query.id });
+
+      if (req.xhr) {
+        return res.status(200).json({
+          data: {
+            post_id: req.query.id,
+          },
+          message: "Post Deleted",
         });
+      }
 
-        if(req.xhr){
-            return res.status(200).json({
-                data:{
-                    post: post
-                },
-                message:"Post Created!"
-            })
-        }
-
-        
-
-        return res.redirect('back');
-    } catch (error) {
-        console.log("Error in creating Post :",error);
-        return;
+      console.log("Post deleted successfully");
+    } else {
+      console.log(
+        "You are not authorised to delete this post | Post not Available"
+      );
     }
-}
-
-module.exports.deletePost = async function(req,res){
-    try {
-        let post =await posts.find({_id : req.query.id});
-        if(post[0].user.toString() == res.locals.user.id){
-            await post[0].deleteOne({_id : req.query.id});
-            await comments.deleteMany({post: req.query.id});
-
-            console.log("Post deleted successfully");
-        }
-        else{
-            console.log("You are not authorised to delete this post | Post not Available");
-        }
-        return res.redirect('back');
-    } catch (error) {
-        console.log("Error in deleting Post",error);
-        return;
-    }
-}
+    return res.redirect("back");
+  } catch (error) {
+    console.log("Error in deleting Post", error);
+    return;
+  }
+};
